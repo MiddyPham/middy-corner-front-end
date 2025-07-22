54e899d10a402edfa51a50ca724e3ba3540ceaf1
MiddyPham <middypham03@gmail.com>
2025-07-22 17:37:35 +0700

    refactor: update code deloy

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -2,7 +2,7 @@ import { sendPost } from "@/lib/axios";
 
 export const authService = {
   login: async (email: string, password: string) => {
-    const response = await sendPost('/auth/login', { email, password });
+    const response = await sendPost('auth/login', { email, password });
     return response;
   },
 };
